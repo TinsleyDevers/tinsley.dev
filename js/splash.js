c19bb9f5d86fd75ee0d68b5f5f2f0d7911597c9a
Tinsley Devers <75707609+TinsleyDevers@users.noreply.github.com>
2023-12-12 20:02:00 -0600

    Update splash.js

--- js/splash.js
+++ js/splash.js
@@ -1,24 +1,19 @@
-// Select the splash and splashText elements
 const splash = document.querySelector(".splash");
 const splashText = document.querySelector(".splash-text");
 
-// Create a typing indicator element
 const typingIndicator = document.createElement("span");
 typingIndicator.classList.add("typing-indicator");
 typingIndicator.textContent = "_";
 let typingTimeout;
 
-// Function to type text one character at a time
 function typeText(text, index = 0) {
   splashText.textContent = text.substring(0, index);
   splashText.appendChild(typingIndicator);
 
-  // Set a timeout to type each character with a random delay
-  typingTimeout = setTimeout(() => {
+ typingTimeout = setTimeout(() => {
     if (index < text.length) {
       typeText(text, index + 1);
     } else {
-      // After typing the text, fade it out and remove the splash screen
       setTimeout(() => {
         let opacity = 1;
         const fadeOutInterval = setInterval(() => {
@@ -26,7 +21,6 @@ function typeText(text, index = 0) {
             clearInterval(fadeOutInterval);
             document.body.removeChild(splash);
 
-            // Change the opacity of the main website elements after removing the splash screen
             const ageText = document.querySelector(".age-text");
             const constructionText =
               document.querySelector(".construction-text");
@@ -45,5 +39,4 @@ function typeText(text, index = 0) {
   }, Math.random() * 200 + 100);
 }
 
-// Call the typeText function to type "{tinsley.dev}" on the splash screen
 typeText("{tinsley.dev}");
