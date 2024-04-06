abf012f2d19f7789d3fcf22e6b1ebaff24594205
Tinsley Devers <Tinsley.devers@Gmail.com>
2024-04-06 07:44:16 -0500

    Updated Code
    Added a console easter egg and changed background color.

--- js/stars.js
+++ js/stars.js
@@ -12,3 +12,20 @@ function createStars() {
 }
 
 createStars();
+
+console.log(
+  "%c\n" +
+    "     CONTACT ME\n" +
+    "   ----------------------------------------\n" +
+    "     .   *   .           *       *  .  *   .\n" +
+    "  *      .      .  *  .  +     *  .     +\n" +
+    " . Tinsley *        *        .     *        *\n" +
+    "     *  .    +   *   .    .  +     .     *    *\n" +
+    "  *        .     +  *  .     *        .      +\n" +
+    "     .  +    .   *   .    .     *        *\n" +
+    "  *       .        *  +  .     .   Devers . *\n" +
+    "     *        .  +     *        *        .   +\n" +
+    "  .     *        .     +  *       *        .\n" +
+    "\nHey There! I'm glad you liked the site. Want to see what's going on? Sure, check the repo at https://github.com/TinsleyDevers/tinsley.dev\n\nAnd don't forget to send me an email at tinsley.devers@gmail.com if you're interested in contacting me to work at your company",
+  "font-family:inherit; font-size: 12px"
+);
