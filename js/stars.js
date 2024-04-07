6909ec82c0ffbc5a4cc400e4adc1dd1635614608
Tinsley Devers <Tinsley.devers@Gmail.com>
2024-04-07 14:08:13 -0500

    Update stars.js

--- js/stars.js
+++ js/stars.js
@@ -26,6 +26,6 @@ console.log(
     "  *       .        *  +  .     .   Devers . *\n" +
     "     *        .  +     *        *        .   +\n" +
     "  .     *        .     +  *       *        .\n" +
-    "\nHey There! I'm glad you liked the site. Want to see what's going on? Sure, check the repo at https://github.com/TinsleyDevers/tinsley.dev\n\nAnd don't forget to send me an email at tinsley.devers@gmail.com if you're interested in contacting me to work at your company",
+    "\nHey There! I'm glad you liked the site. Want to see what's going on? check out the repo at https://github.com/TinsleyDevers/tinsley.dev\n\nAlso, you can contact me via tinsley.devers@gmail.com if you have any questions!",
   "font-family:inherit; font-size: 12px"
 );
