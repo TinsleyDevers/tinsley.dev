8a1a3b3a1abb52e12b224e363f8b2b21674ded0d
Tinsley Devers <Tinsley.devers@Gmail.com>
2024-04-10 06:39:12 -0500

    Update parallax.js
    Disabled for mobile devices.

--- js/parallax.js
+++ js/parallax.js
@@ -1,26 +1,28 @@
 function parallaxEffect() {
-  const ageText = document.querySelector(".age-text");
-  const constructionText = document.querySelector(".construction-text");
-  const scrollIndicator = document.querySelector(".scroll-down-indicator");
-  const headname = document.querySelector(".headname");
+  if (window.innerWidth > 1024) {
+    const ageText = document.querySelector(".age-text");
+    const constructionText = document.querySelector(".construction-text");
+    const scrollIndicator = document.querySelector(".scroll-down-indicator");
+    const headname = document.querySelector(".headname");
 
-  const headnameSpeed = 0.8;
-  const ageTextSpeed = 0.47;
-  const constructionTextSpeed = 0.51;
-  const scrollIndicatorSpeed = 0.28;
+    const headnameSpeed = 0.8;
+    const ageTextSpeed = 0.47;
+    const constructionTextSpeed = 0.51;
+    const scrollIndicatorSpeed = 0.28;
 
-  const scrollY = window.scrollY;
+    const scrollY = window.scrollY;
 
-  headname.style.transform = `translateY(${scrollY * headnameSpeed}px)`;
-  ageText.style.transform = `translateY(${scrollY * ageTextSpeed}px)`;
-  constructionText.style.transform = `translateY(${
-    scrollY * constructionTextSpeed
-  }px)`;
-  scrollIndicator.style.transform = `translateY(${
-    scrollY * scrollIndicatorSpeed
-  }px)`;
+    headname.style.transform = `translateY(${scrollY * headnameSpeed}px)`;
+    ageText.style.transform = `translateY(${scrollY * ageTextSpeed}px)`;
+    constructionText.style.transform = `translateY(${
+      scrollY * constructionTextSpeed
+    }px)`;
+    scrollIndicator.style.transform = `translateY(${
+      scrollY * scrollIndicatorSpeed
+    }px)`;
 
-  requestAnimationFrame(parallaxEffect);
+    requestAnimationFrame(parallaxEffect);
+  }
 }
 
 document.addEventListener("DOMContentLoaded", parallaxEffect);
